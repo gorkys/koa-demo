a3a7039f0f1da2f7de43a8a3cce0e874a210a748
gorkys <king@tingtas.com>
2019-09-03 16:06:51 +0800

    add: 为Sequelzie添加时间格式化

--- server/config/db.js
+++ server/config/db.js
@@ -8,7 +8,12 @@ const demo = new Sequelize(`mysql://${process.env.DB_USER}:${process.env.DB_PASS
     define:{
         timestamps: false // 取消Sequelzie自动给数据表加入时间戳（createdAt以及updatedAt）
     },
-    timezone: '+08:00' // 东八时区
+    timezone: '+08:00', // 东八时区
+    // 时间格式化 see: https://segmentfault.com/a/1190000020009630
+    dialectOptions: {
+        dateStrings: true,
+        typeCast: true
+    }
 })
 
 export default demo
